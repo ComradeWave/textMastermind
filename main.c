ac7720511ace693ac6a3a73993f5e6a387f00480
ComradeWave <94688733+ComradeWave@users.noreply.github.com>
2023-04-13 11:55:39 +0200

    COMMIT

--- main.c
+++ main.c
@@ -2,14 +2,63 @@
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
-
- 
+#include <stdbool.h>
 #define MAX 4
-void casualitygenerator(int game[]){
+
+
+bool gamecontrol(int *perfection, int *presence, int game[], int gamecompare[], int trials){
+	int x, y; //servizio
+	for(x=0;x<MAX;x++){
+		for(y=0;y<MAX;y++){
+			if(gamecompare[y]==game[x]){ //controllo VALORE degli array per determinare la presenza
+				*presence=*presence+1;
+				//printf("%d DBEUGGY\n", *presence);
+				if(x==y){
+					*perfection=*presence+1; //posizione e valore uguale
+				//	printf("%d DBEUGGYA\n", *perfection);				
+				}
+			}
+		}
+	}
+	if(*perfection==MAX){
+		return 1;
+	}else{
+		printf("#%d\t\t%d\t%d\t%d\t%d \t Numeri Presenti: %d Numeri corretti: %d\n", trials, gamecompare[0], gamecompare[1], gamecompare[2], gamecompare[3], presence, perfection);
+		return 0;
+	}
+}
+
+
+
+bool gameins(int game[], int gamecompare[], int trials){
+	int index; //servizio
+	int perfection=0,presence=0;
+	bool error=false;
+	do{
+		printf("\nInserisci i numeri: "); 
+		scanf("%d %d %d %d", &gamecompare[0], &gamecompare[1], &gamecompare[2], &gamecompare[3]);
+		for(index=0;index<MAX;index++){
+			if(gamecompare[index]<1 || gamecompare[index]>6){
+				printf("INPUT ERRATO\n");
+				error=true;
+			}else{
+				error=false;
+			}
+		}
+	}while(error==true);
+	if(gamecontrol(&perfection, &presence, game, gamecompare, trials)==true){
+		return 1;
+	}else{
+		return 0;
+	}
+	
+}
+
+void casualitygenerator(int game[]){ //Crea numeri casuali
 	int i;
 	srand(time(NULL));
 	for(i=0;i<MAX;i++){
-		game[i]=rand() % 7;
+		game[i]=rand() % 6+1;
 		printf("%d DEBUG\n", game[i]);
 	}
 	return;
@@ -17,42 +66,32 @@ void casualitygenerator(int game[]){
 
 void gamestart(){ //manuale, regole del gioco
 	printf("\nHai 10 tentativi per indovinare 4 numeri!\n");
-	printf("\nEsempio: #1\t1, 2, 3, 4\t2p 1c\n");
-	printf("\nP, C: Presente e Corretto\n");
+	printf("\nEsempio: #1\t1, 2, 3, 4");
+//	printf("\nP, C: Presente e Corretto\n");
 	printf("\nBuona Fortuna!\n");	
-	sleep(10);
-	printf("\e[1;1H\e[2J");
+	sleep(3);
+	printf("\e[1;1H\e[2J"); //REGEX
 	return;
 }
 
-void gameplayer(int game[], int gamecompare[]){ //qui avviene il gioco
+void gameplayer(int game[], int gamecompare[]){ //qui starta il gioco
 	int trials=10;
-	int i,j,o; //servizio
-	int perfection=0,presence=0;
+	
+	
 	do{
-		printf("\nInserisci i dati: "); //da rimpiazzare
-		for(o=0;o<MAX;o++) scanf("%d",&gamecompare[o]); //ottimizzato
-		for(i=0;i<MAX;i++){
-			for(j=0;j<MAX;j++){
-				if(game[i]==gamecompare[j]){
-					presence++;
-					if(i==j){
-						perfection++;
-					}else if(perfection>0){
-						perfection--;
-					}
-				}else if(presence>0){
-					presence--;
-				}
-			}
+		
+			
+		if(gameins(game, gamecompare, trials)==true){
+			printf("HAI VINTOOO!\n");
+			return;
 		}
-		printf("\n#%d\t%d %d %d %d\tP:%d C:%d",trials,gamecompare[0],gamecompare[1],gamecompare[2],gamecompare[3],presence,perfection);
+		
 		trials--;
 	}while(trials!=0);
 }
 int main(){
-	int game[MAX];
-	int gamecompare[MAX];
+	int game[MAX]; //array dove ci sono i numeri casuali
+	int gamecompare[MAX]; //dove il player mette i numeri
 	casualitygenerator(game);
 	gamestart();
 	gameplayer(game, gamecompare);
